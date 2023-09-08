77a1d7b41c06ba45ef19939eb7ccd5eebf3cef03
Mehmet Yasin Cifci <cifci@campus.tu-berlin.de>
2023-09-08 15:42:31 +0200

    added critic

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# from typing import Iterable, Tuple
+import mlflow
+from mlflow import log_metric
+
+import random
 
 import numpy as np
 import torch
 import torchvision.transforms as T
 from torch import nn
 from torch.utils.data.dataloader import DataLoader
-from torchvision.models.resnet import ResNet34_Weights, resnet34, ResNet152_Weights, resnet152, resnet50, ResNet50_Weights, resnet101
+from torchvision.models.resnet import resnet34
 from tqdm import tqdm
 from wilds import get_dataset
 from wilds.common.data_loaders import get_eval_loader, get_train_loader
 from wilds.common.grouper import CombinatorialGrouper
 
 from model import DANN
-
-import random
-
 
 def validate(model: nn.Module, val_loader: DataLoader, device: str) -> float:
     model.eval()
@@ -23,17 +23,46 @@ def validate(model: nn.Module, val_loader: DataLoader, device: str) -> float:
     with torch.no_grad():
         correct = 0
 
-        for x, y, _ in tqdm(val_loader):
-            x, y = x.to(device), y.to(device)
+        for x, t, _ in tqdm(val_loader):
+            x, t = x.to(device), t.to(device)
 
-            y_ = model(x)
-            correct += (y == y_.argmax(axis=1)).sum().item()
+            y, d = model(x)
+            correct += (t == y.argmax(axis=1)).sum().item()
 
         return correct/num_samples_val
 
 
+def train_one_epoch(model, train_loader, grouper, optimizer, criterion, device):
+    crit_crit = nn.NLLLoss()
+
+    for i, (x, t, metadata) in enumerate(pbar := tqdm(train_loader)):
+            z = grouper.metadata_to_group(metadata)
+
+            x, t, z = x.to(device), t.to(device), z.to(device)
+
+            optimizer.zero_grad()
+
+            y, d = model(x)
+            loss_clsf = criterion(y, t)
+            loss_crit = crit_crit(d, z)
+            loss = loss_clsf + loss_crit
+
+            loss.backward()
+            
+            optimizer.step()
+
+            batch_accuracy = (y.argmax(axis=1) == t).sum().item()/len(t)
+            batch_dom_accuracy = (d.argmax(axis=1) == z).sum().item()/len(t)
+
+            pbar.set_description(f"Cl.-acc.: {batch_accuracy:.4f}, Dom.-acc.: {batch_dom_accuracy}")
+
+            log_metric("clsf_loss", loss_clsf.item(), i)
+            log_metric("crit_loss", loss_crit.item(), i)
+            log_metric("train_loss", loss.item(), i)
+
 def main():
     random.seed(42)
+    np.random.seed(42)
     torch.manual_seed(42)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -64,48 +93,33 @@ def main():
 
     # Prepare the standard data loader
     train_loader = get_train_loader(
-        "group", train_data, batch_size=3*16, grouper=grouper, n_groups_per_batch=3)
+        "group", train_data, batch_size=3*395, grouper=grouper, n_groups_per_batch=3)
 
-    # model = resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
-    # model.fc = nn.Linear(in_features=512, out_features=2, bias=True)
-    # model.to(device)
-
-    model = resnet101()
-    model.fc = nn.Linear(in_features=2048, out_features=2, bias=True)
+    model = DANN()
     model.to(device)
 
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-3, weight_decay=1e-3)
+    optimizer = torch.optim.AdamW(
+        params=model.parameters(), lr=4e-4)
+
+    # optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=1e-3, weight_decay=1e-3)
+
     criterion = torch.nn.CrossEntropyLoss().to(device)
 
     # Train loop
     for epoch in range(2):
-        i = 0
-        losses = []
-        for x, t, metadata in tqdm(train_loader):
-            z = grouper.metadata_to_group(metadata)
+        train_one_epoch(
+             model=model,
+             train_loader=train_loader,
+             grouper=grouper,
+             optimizer=optimizer,
+             criterion=criterion,
+             device=device
+        )
 
-            x, t = x.to(device), t.to(device)
-
-            optimizer.zero_grad()
-
-            y = model(x)
-            loss = criterion(y, t)
-
-            loss.backward()
-            optimizer.step()
-
-            losses.append(loss.item())
-            
-            if i % 100 == 0:
-                print(f"Avg. train-loss: {np.array(losses).mean()}")
-                losses = []
-                
-            i += 1
-
-    val_loader = get_eval_loader("standard", val_data, batch_size=3*128)
+    val_loader = get_eval_loader("standard", val_data, batch_size=3*395)
 
     eval_acc = validate(model, val_loader, device)
-    print(f"Eval. accuracy: {eval_acc}")
+    print(f"Eval. accuracy: {eval_acc:.4f}")
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -2,21 +2,41 @@ from typing import Tuple
 
 import torch
 from torch import nn
-from torchvision.models.resnet import ResNet18_Weights, resnet18
+from torchvision.models.resnet import resnet34
 
+import numpy as np
+
+from torch.autograd import Function
+
+
+class ReverseLayerF(Function):
+
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
 
 class DANN(nn.Module):
     def __init__(self) -> None:
         super(DANN, self).__init__()
-        self.backbone = resnet18()
-        self.fc = nn.Linear(in_features=512, out_features=2)
-        self.domain_classifier = nn.Linear(in_features=512, out_features=2)
+        self.backbone = resnet34()
+        self.backbone.fc = nn.Linear(in_features=512, out_features=2)
+        self.domain_classifier = nn.Linear(in_features=512, out_features=5)
 
     def forward(self, x) -> Tuple[torch.Tensor]:
         for l in list(self.backbone.children())[:-1]:
             x = l(x)
+        f = x.squeeze()
+        f_r = ReverseLayerF.apply(f, 0.8)
 
-        y = self.backbone.fc(x)
-        d = self.domain_classifier(x)
+        y = self.backbone.fc(f)
+        d = self.domain_classifier(f_r)
 
         return (y, d)
